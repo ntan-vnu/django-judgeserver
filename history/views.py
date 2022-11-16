38bd1f21936edc845b7b2c3d865a0e32a1d55443
Thanh-An NGUYEN <nguyenthanhan1001@gmail.com>
2022-11-16 21:52:39 +0700

    update admin site

--- history/views.py
+++ history/views.py
@@ -8,16 +8,16 @@ from judge.models import Submission, Result
 def history(request: HttpRequest) -> HttpResponse:
     if request.user.is_superuser:
         currentClass = ClassProfile.objects.get(active=True)
-        smss = Submission.objects \
-            .filter(exercise__lab__classCode=currentClass).all() \
+        submissions = Submission.objects \
+            .filter(student__classCode=currentClass).all() \
             .order_by('-sDatetime')
         context = {
             'cls': currentClass,
             'labs': currentClass.labs.all(),
             'results': Result.getResultsByClass(currentClass),
-            'submissions': smss
+            'submissions': submissions
         }
-        return render(request, 'history.html', context=context)
+        return render(request, 'history/history.html', context=context)
     else:
         return HttpResponse('Admin only.')
 
