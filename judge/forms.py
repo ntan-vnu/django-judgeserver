4217b4ec92ba85d008eabcde7417a18f3d89a548
Thanh-An NGUYEN <nguyenthanhan1001@gmail.com>
2022-12-01 17:09:52 +0700

    relayout

--- judge/forms.py
+++ judge/forms.py
@@ -4,13 +4,11 @@ from judge.models import Exercise, Submission
 
 
 class SubmitForm(forms.ModelForm):
-
     class Meta:
         model = Submission
         fields = ['exercise', 'source']
-        
-    def __init__(self, *args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.fields['exercise'].queryset = Exercise.objects.filter(
             lab__classCode__active=True)
-
