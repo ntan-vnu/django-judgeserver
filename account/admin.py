d3d90eba14c08dbae6c5f2b4cb5cb1d0a273558e
Thanh-An NGUYEN <nguyenthanhan1001@gmail.com>
2022-12-01 17:16:55 +0700

    udpate admin site of account

--- account/admin.py
+++ account/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from account.models import *
 
-admin.site.register(ClassProfile)
-admin.site.register(StudentProfile)
+
+class ClassProfileAdmin(admin.ModelAdmin):
+    list_display = ['code', 'course', 'active']
+
+
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ['studentID', 'fullName', 'classCode']
+
+
+admin.site.register(ClassProfile, ClassProfileAdmin)
+admin.site.register(StudentProfile, StudentProfileAdmin)
